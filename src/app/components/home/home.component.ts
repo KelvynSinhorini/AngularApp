c67bd55c88aaeab100fbba24127f0dc59e97a7a3
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-06-16 22:06:17 -0300

    feat: consumir fornecedor service no home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FornecedorService } from '../../services/fornecedor/fornecedor.service';
+import { Fornecedor } from '../../services/fornecedor/fornecedor';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ CommonModule, RouterLink ],
+  imports: [ 
+    CommonModule, 
+    RouterLink
+  ],
+  providers: [
+    FornecedorService
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  fornecedores: Fornecedor[] = [];
 
+  constructor(private fornecedorService: FornecedorService) { }
+
+  ngOnInit() {
+    this.fornecedorService
+      .obterFornecedores()
+      .subscribe({
+        next: (data: Fornecedor[]) => {
+          this.fornecedores = data;
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      })
+  }
 }
