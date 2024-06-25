a31e4f01a6a92d99d9e98f20246ed345ff9b06e2
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-06-25 17:02:59 -0300

    feat: adicionar navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
 })
